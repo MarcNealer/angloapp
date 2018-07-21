d7b2d064ec41c1df7801edfb320d63a881c90379
Marc Nealer <marc@willowtreesoftware.com>
2018-07-21 07:50:46 +0700

    removed print statement

--- grupos/db_signal.py
+++ grupos/db_signal.py
@@ -1,7 +1,6 @@
 from history.utils import history_add
 
 def save_history(sender, instance, **kwargs):
-    print('signal met')
     if kwargs['created']:
         history_add(instance.grupo, instance.alumno,
                     "New Membership. Status: {}".format(instance.status))
